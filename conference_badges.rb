59bb369822b0b1167e7cf4d512ec297dabee8e6b
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-20 21:11:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_maker(badges)
+def batch_badge_creator(badges)
   attendees = []
   attendees.each do |name|
     "Hello, my name is #{name}."
